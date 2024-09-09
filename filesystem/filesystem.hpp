b5700d9f18ba1e7372dc294510105f98b611045b
koldomasov.nd <koldomasov.nd@dozen.mephi.ru>
2024-09-09 22:44:29 +0300

    Document filesystem.hpp

--- filesystem/filesystem.hpp
+++ filesystem/filesystem.hpp
@@ -6,9 +6,55 @@
 
 namespace Filesystem {
 
+/*!
+ * \brief Joins a base path with an appended path.
+ *
+ * \param base The base path.
+ * \param appended The path to append.
+ * \return A string representing joined path.
+ * \throws std::invalid_argument If appended is absolute.
+ * \throws std::invalid_argument If base or appended are empty.
+ */
 std::string join(std::string_view base, std::string_view appended);
+
+/*!
+ * \brief Converts a given path to an absolute path.
+ *
+ * Converts a given path to an absolute path with respect to
+ * the current directory.
+ * If paht is already absolute, does nothing except formatting.
+ *
+ * \param path The path to convert.
+ * \return A string representing absolute path.
+ * \throws std::invalid_argument If path is empty.
+ */
 std::string absolute(std::string_view path);
+
+/*!
+ * \brief Relativizes a target path with respect to a base path.
+ *
+ * Converts a target path to a relative path with respect to
+ * the base path.
+ *
+ * \param base The base path.
+ * \param path The path to relativize.
+ * \return A string representing relativized path.
+ * \throws std::invalid_argument If base or path are absolute.
+ * \throws std::invalid_argument If base or path are empty.
+ */
 std::string relativize(std::string_view base, std::string_view path);
+
+/*!
+ * \brief Converts a given path to a relative path.
+ *
+ * Converts a given path to a relative path with respect to
+ * the current working directory.
+ * If path is already relative, does nothing except formatting.
+ *
+ * \param path The path to relativize.
+ * \return A string representing relativized path.
+ * \throws std::invalid_argument If path is empty.
+ */
 std::string relative(std::string_view path);
 
 }  // namespace Filesystem
